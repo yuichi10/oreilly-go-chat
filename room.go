22d972f652885f9f354bd6007a458a8c73cb40c6
yuichi10 <y_084743@yahoo.co.jp>
2018-03-09 18:33:29 +0900

    1.2.3 room has what he do

--- room.go
+++ room.go
@@ -10,3 +10,27 @@ type room struct {
 	// clients には在室している全てのクライアントが保持される
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// すべてのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセ＝時を送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}

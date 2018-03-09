88f296fdbb5745b29e5b2a4f94e07f5a2d1a791a
yuichi10 <y_084743@yahoo.co.jp>
2018-03-09 18:24:54 +0900

    1.2.2 fix room struct

--- room.go
+++ room.go
@@ -3,4 +3,10 @@ package main
 type room struct {
 	// forward は他のクライアントに転送するためのメッセージを保持するチャネルです
 	forward chan []byte
+	// joinはチャットルームに参加しようとしているクライアントのためのチャネルです。
+	join chan *client
+	// leaveはチャットルームから退出しようとしているクライアントのためのチャネル
+	leave chan *client
+	// clients には在室している全てのクライアントが保持される
+	clients map[*client]bool
 }
